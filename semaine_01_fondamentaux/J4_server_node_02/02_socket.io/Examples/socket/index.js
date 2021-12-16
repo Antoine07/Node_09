4a3d04f1eba967fbab8e182555443f25003edaff
Antoine07 <antoine.lucsko@gmail.com>
2021-12-16 17:26:07 +0100

    socket.io

--- semaine_01_fondamentaux/J4_server_node_02/02_socket.io/Examples/socket/index.js
+++ semaine_01_fondamentaux/J4_server_node_02/02_socket.io/Examples/socket/index.js
@@ -1,8 +1,10 @@
 const http = require("http");
 const fs = require('fs');
+
+// Un server qui sera branché en permanence avec le client
 const { Server } = require("socket.io");
 
-const hostname = 'localhost';
+const hostname = '192.168.1.101';
 const port = '8080';
 
 const server = (req, res) => {
@@ -26,14 +28,16 @@ const io = new Server(app);
 
 // connection de socker io avec notre server Node.js
 io.on('connection', (socket) => {
+    // est ce que le client envoie quelque chose ? Si oui on récupère le message
     socket.on('chat message', (msg) => {
         console.log('message: ' + msg);
-        // envoyer un message à tout le monde
+        // re-envoyer un message à tout le monde, même à celui qui l'a envoyé
         io.emit('chat message', msg);
 
         // envoyer un message sauf à celui qui a déclenché l'événement
-        socket.broadcast.emit('some event');
+        socket.broadcast.emit('chat message', 'SOME EVENT !!');
     });
+
     socket.on('disconnect',  () => {
         console.log('Disconnect');
     });
